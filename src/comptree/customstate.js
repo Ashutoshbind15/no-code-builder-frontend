dc1e20347ed6260c74659369e8ffab8484f630ac
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2025-07-24 21:18:37 +0530

    get a better state representation, and a better logic/way to resolve props

--- src/comptree/customstate.js
+++ src/comptree/customstate.js
@@ -4,6 +4,7 @@ export const customStarterState = {
         type: "div",
         // keeping the effects and state on the top level while rendering, while building, push it down the tree and optimize
         effects: [],
+        actions: [],
         state: [],
         props: [],
         children: [
@@ -14,6 +15,13 @@ export const customStarterState = {
                     name: "className",
                     value: "w-1/4 bg-red-500"
                 }],
+                effects: [],
+                actions: [],
+                state: [{
+                    name: "ctrval",
+                    type: "number",
+                    initValue: 0
+                }],
                 children: [
                     {
                         id: "sidebar-button",
@@ -22,18 +30,21 @@ export const customStarterState = {
                             name: "className",
                             value: "bg-blue-500"
                         }],
+                        // todo: update the hof in acc to the new param
                         actions: [{
                             trigger: "onClick",
-                            actionParams: ["mutatestate", "ctrval", "inc"]
+                            actionParams: ["mutatestate", "sidebar", "ctrval", "inc"]
                         }],
                         children: [{
                             id: "sidebar-button-text",
-                            type: "stringLiteral",
+                            type: "predef:Text",
                             props: [{
                                 name: "text",
                                 value: "Click me"
                             }]
-                        }]
+                        }],
+                        effects: [],
+                        state: []
                     }
                 ]
             },
@@ -44,6 +55,9 @@ export const customStarterState = {
                     name: "className",
                     value: "w-3/4 bg-green-500"
                 }],
+                effects: [],
+                actions: [],
+                state: [],
                 children: [
                     {
                         id: "main-content-card",
@@ -77,34 +91,19 @@ export const customStarterState = {
                                 children: [
                                     {
                                         id: "main-content-container-child-div-child-p",
-                                        type: "p",
+                                        type: "predef:Text",
                                         props: [{
                                             name: "className",
                                             value: "text-sm text-gray-500"
-                                        }],
-                                        children: [{
-                                            id: "main-content-container-child-div-child-p-text",
-                                            type: "stringLiteral",
-                                            props: [{
-                                                name: "text",
-                                                value: "state:ctrval"
-                                            }]
-                                        },
-                                        {
-                                            id: "random-text",
-                                            type: "predef:Text",
-                                            props: [{
-                                                name: "className",
-                                                value: "text-sm text-gray-500"
-                                            }, {
-                                                name: "text",
-                                                value: "userdef in userdef"
-                                            }],
-
-                                        }
-                                        ]
+                                        }, {
+                                            name: "text",
+                                            value: "state:sidebar:ctrval"
+                                        }]
                                     }
-                                ]
+                                ],
+                                effects: [],
+                                actions: [],
+                                state: []
                             }
                         ]
                     }
@@ -117,21 +116,16 @@ export const customStarterState = {
                     value: "section title"
                 }],
                 children: [{
-                    id: "section-aside-blankspace",
-                    type: "h1",
+                    id: "section-aside-blankspace-text",
+                    type: "predef:Text",
                     props: [{
                         name: "text",
-                        value: "inserted child"
-                    }],
-                    children: [{
-                        id: "section-aside-blankspace-text",
-                        type: "stringLiteral",
-                        props: [{
-                            name: "text",
-                            value: "prop:text"
-                        }]
+                        value: "prop:section-aside:title"
                     }]
-                }]
+                }],
+                state: [],
+                actions: [],
+                effects: []
             }
         ]
 
@@ -141,12 +135,39 @@ export const customStarterState = {
 export const customComponentsMetadata = {
     "Card": {
         takesChildren: false,
+        defaultProps: [{
+            name: "title",
+            type: "string",
+            value: "Hello there",
+        }, {
+            name: "description",
+            type: "string",
+            value: "this is a description"
+        }, {
+            name: "image1",
+            type: "string",
+            value: "https://images.example.com/150"
+        }]
     },
     "Container": {
-        takesChildren: true
+        takesChildren: true,
+        defaultProps: [{
+            name: "className",
+            type: "string",
+            value: "bg-red-500"
+        }]
     },
     "Text": {
-        takesChildren: false
+        takesChildren: false,
+        defaultProps: [{
+            name: "text",
+            type: "string",
+            value: "Enter here"
+        }, {
+            name: "className",
+            type: "string",
+            value: "text-sm text-gray-500"
+        }]
     },
 }
 
@@ -155,6 +176,7 @@ export const userDefComps = {
     "Section": {
         type: "div",
         takesChildren: true,
+        // means we can add elements to the body array
         props: [{
             name: "className",
             defaultValue: "bg-blue-500"
@@ -162,28 +184,67 @@ export const userDefComps = {
             name: "title",
             defaultValue: "Hello"
         }],
-        body: [{
-            id: "userseccard",
-            type: "predef:Card",
-            props: [{
-                name: "title",
-                value: "prop:title"
-            }, {
-                name: "description",
-                value: "section description"
-            }, {
-                name: "image1",
-                value: "https://example.com/images/150"
-            }]
-        }, {
-            id: "usersecaside",
-            type: "div",
-            props: [],
-            children: [{
-                id: "usersecaside-blankspace",
-                type: "children",
-                props: []
-            }]
-        }]
+        state: [],
+        effects: [],
+        actions: [],
+        children: [
+            {
+                // ids inside of body dont make sense, as body is just for the sake of structure, 
+                // ids should be declared at runtime, when the custom/usercomp is used inside of the 
+                // dnd editor
+                // id: "userseccard",
+                type: "predef:Card",
+                props: [{
+                    name: "title",
+                    value: "prop:{Section}:title"
+                }, {
+                    name: "description",
+                    value: "section description"
+                }, {
+                    name: "image1",
+                    value: "https://example.com/images/150"
+                }],
+            },
+            {
+                // id: "usersecaside",
+                type: "div",
+                props: [],
+
+                //redundant to add children like this, as we'll allow the end user to add in another 
+                // components to the body itself
+
+                // children: [{
+                //     id: "usersecaside-blankspace",
+                //     type: "children",
+                //     props: []
+                // }],
+
+                // later, just allow the user to add stuff to it, have a flag or something, that indicates containers/ the comps that take in 
+                // children
+                children: []
+            }
+        ]
+    },
+    "PageDiv": {
+        type: "div",
+        takesChildren: true,
+        state: [],
+        props: [],
+        children: [],
+        effects: [],
+        actions: []
     }
 }
+
+// finally, when a userdef comp is added, the dynamic props: {} are populated recursively
+// section -> card -> title
+// section is being defined : props: [title]
+// card: props[cardtitle]: val -> props:{section}:title 
+// title: <text> tag : props[text] : val -> props:{card}:cardtitle
+
+// then when it's used, populate with the id, after traversing the tree
+// section -> id: 123
+// card -> id: xyz, prop : props:123:title
+// title -> id: abx, prop: props:xyz:cardtitle
+
+// also take care that the nesting could be well inside, so even the n+2th child should be able to track the nearest {dyn-parent-assigned-id}
