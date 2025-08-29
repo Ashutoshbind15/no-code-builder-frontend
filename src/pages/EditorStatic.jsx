ebea9821d18483d8fbfeec82b94de53d9ddf5d6b
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2025-08-29 22:23:02 +0530

    add some plans for controlling prop metadata

--- src/pages/EditorStatic.jsx
+++ src/pages/EditorStatic.jsx
@@ -19,6 +19,18 @@ const starterLiteralValues = {
     "p:Literal:0.1": "Hello, World!"
 }
 
+// where exactly do we want the defaults for the props to resolve?
+
+// do we define a map of all the porps that a component takes, make them compulsary at the component level, and at runtime only, 
+// do we define and pass all of them, acc to the defaults defined pre-hand...
+
+// or do we also allow default props on the code level, so we dont necessarily need to pass all the props while creating/adding at runtime...
+
+//any of the two, but one thing must not be allowed, i.e. adding a component without a compulsary prop...
+
+// first one seems more logical, as it'd give predictable errors, and we anyways need to track the props
+
+
 // for now the state change is controlled by the sidebar,
 // later, when dnd, select tracking, and mouse events are added,
 // we can change the tree state from within the renderer as well,
